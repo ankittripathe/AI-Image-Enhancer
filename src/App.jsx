c1633699bf024c35d9245f8c61037abdee6e31e0
Dhoni <ankitdhonee@gmail.com>
2025-04-11 23:31:43 +0545

    setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,12 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router";
-import Home from './Pages/Home'
-import About from './Pages/About'
-import Contact from './Pages/Contact'
-import Menu from './Pages/Menu'
-import PageNotFound from './Pages/PageNotFound'
-
+import React from 'react'
 
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>}/>
-          <Route path="/contact" element={<Contact/>}/>
-          <Route path="/menu" element={<Menu/>}/>
-          <Route path="*" element={<PageNotFound/>}/>
-        </Routes>
-      </BrowserRouter>
+        <h1>AI Image Enhancer</h1>
+      
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
