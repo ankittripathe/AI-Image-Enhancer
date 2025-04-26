e0019b7a38c0ebe20b1f6d7ce32fe5e84f6aa6d2
Dhoni <ankitdhonee@gmail.com>
2025-04-26 17:22:07 +0545

    all ok for now

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,30 +3,26 @@ import ImageUpload from "./ImageUpload";
 import ImagePreview from "./ImagePreview";
 import enhancedImageAPI from "../utils/enhancedImageApi";
 
-
 const Home = () => {
   const [uploadImage, setUploadImage] = useState(null);
   const [enhancedImage, setEnhancedImage] = useState(null);
   const [loading, setLoading] = useState(false);
-
 
   const uploadImagehandler = async (file) => {
     console.log(URL.createObjectURL(file)); // console
     setUploadImage(URL.createObjectURL(file));
     setLoading(true);
 
-
-    // try {
-    //   // code which may produce error
-    //   const enhancedURL = await enhancedImageAPI(file);
-    //   setEnhancedImage(enhancedURL);
-    //   setLoading(false);
-    // } catch (error) {
-    //   // code to handle the error and show message
-    //   console.log(error);
-    //   alert("Error while enhancing the image. Please try again later");
-    // }
-
+    try {
+      // code which may produce error
+      const enhancedURL = await enhancedImageAPI(file);
+      setEnhancedImage(enhancedURL);
+      setLoading(false);
+    } catch (error) {
+      // code to handle the error and show message
+      console.log(error);
+      alert("Error while enhancing the image. Please try again later");
+    }
   };
 
   return (
